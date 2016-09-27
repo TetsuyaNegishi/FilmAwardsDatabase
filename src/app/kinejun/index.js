01f2edf00d4c08293fd0fce7a74c11e94538f0b0
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2016-09-27 21:52:54 +0900

    [add]絞込検索

--- src/app/kinejun/index.js
+++ src/app/kinejun/index.js
@@ -6,4 +6,64 @@ const kinejun = angular
     .module('kinejun',[])
     .component('kinejun', kinejunComponent);
 
+// 受賞年用のフィルタ
+kinejun.filter('yearStart', function() {
+    return function(input, from) {
+        if(!from) {
+            return input;
+        }
+        var arr = [];
+        input.some(function(v, i) {
+            if (v.year >= from) {
+                arr.push(input[i]);
+            }
+        });
+        return arr;
+    };
+});
+kinejun.filter('yearEnd', function() {
+    return function(input, to) {
+        if(!to) {
+            return input;
+        }
+        var arr = [];
+        input.some(function(v, i) {
+            if (v.year <= to) {
+                arr.push(input[i]);
+            }
+        });
+        return arr;
+    };
+});
+
+// ランク用のフィルタ
+kinejun.filter('rankStart', function() {
+    return function(input, from) {
+        if(!from) {
+            return input;
+        }
+        var arr = [];
+        input.some(function(v, i) {
+            if (v.rank >= from) {
+                arr.push(input[i]);
+            }
+        });
+        return arr;
+    };
+});
+kinejun.filter('rankEnd', function() {
+    return function(input, to) {
+        if(!to) {
+            return input;
+        }
+        var arr = [];
+        input.some(function(v, i) {
+            if (v.rank <= to) {
+                arr.push(input[i]);
+            }
+        });
+        return arr;
+    };
+});
+
 export default kinejun;
